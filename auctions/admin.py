fc55f3f0a4af7e2b183816772a1799c0b344ea30
dinae <dina.erchenko@gmail.com>
2022-02-09 15:35:34 +0300

    add model for listings

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import AbstractUser, AuctionListing
+
 # Register your models here.
+admin.site.register(AuctionListing)
--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,17 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class AuctionListing(models.Model):
+    """
+    Model for auction listings.
+    User (seller) add listing
+    """
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    starting_bid = models.DecimalField(max_digits=8, decimal_places=2)
+    seller = models.ForeignKey(User, on_delete=models.CASCADE, related_name="items")
+
+    def __str__(self):
+        return f'{self.id}: {self.title} - {self.starting_bid}'
