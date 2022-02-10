cd01cd669cc92548b9be9a0202635ce9991f34dd
dinae <dina.erchenko@gmail.com>
2022-02-10 13:24:06 +0300

    add bids model

--- auctions/models.py
+++ auctions/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.validators import MinValueValidator
+from decimal import Decimal
 
 
 class User(AbstractUser):
@@ -13,8 +15,19 @@ class AuctionListing(models.Model):
     """
     title = models.CharField(max_length=64)
     description = models.TextField()
+    img_url = models.URLField('Image', blank=True)
     starting_bid = models.DecimalField(max_digits=8, decimal_places=2)
     seller = models.ForeignKey(User, on_delete=models.CASCADE, related_name="items")
 
     def __str__(self):
         return f'{self.id}: {self.title} - {self.starting_bid}'
+
+
+class Bids(models.Model):
+    """
+    Model for user's bids
+    """
+    value = models.DecimalField(max_digits=6, decimal_places=2)
+    listing = models.ForeignKey(AuctionListing, on_delete=models.CASCADE, related_name="bids")
+    bidder = models.ForeignKey(User, on_delete=models.CASCADE)
+
